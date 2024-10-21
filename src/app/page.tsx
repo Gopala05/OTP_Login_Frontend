549a06cda72f9a795c9310233df56ad800d2cfb2
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-10-22 00:05:26 +0530

    UI update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import LoginPopUp from "@/component/PopUp/loginPopUp";
+// Library Imports
 import { useEffect, useState } from "react";
-import Modal from "react-modal";
 
-export default function Home() {
+// Custom Imports
+import LoginPopUp from "@/component/PopUp/loginPopUp";
+
+const Home = () => {
   const [showLogin, setShowLogin] = useState<boolean>(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    const appElement = document.getElementById("__next") || document.body;
-    Modal.setAppElement(appElement);
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button onClick={() => setShowLogin(true)}>Login</button>
+    <div className="flex h-screen w-full justify-center items-center bg-black">
+      <button
+        className="bg-[#2bb8a4] text-4xl py-4 px-6 rounded-lg text-white font-semibold hover:bg-[#2bb8a4] hover:shadow-lg hover:scale-105 hover:shadow-black/20 transition-colors duration-200 disabled:bg-[#7A7676] disabled:cursor-not-allowed disabled:scale-100 disabled:shadow-none"
+        onClick={() => setShowLogin(true)}
+      >
+        Login
+      </button>
 
       {/* Login Modal */}
       <LoginPopUp open={showLogin} setClose={() => setShowLogin(false)} />
     </div>
   );
-}
+};
+
+export default Home;
